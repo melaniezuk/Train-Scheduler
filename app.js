aa9d47808cdde9ed2cfe0552623314904aa3e07e
MelZee <zenani3@gmail.com>
2018-06-12 14:37:23 -0400

    added css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* global moment firebase */
-     
-var config = {
+$(document).ready(function () {
+  var config = {
   apiKey: "AIzaSyBNxWXreeyxxf58pPO04IGp3LyvOYz3b6o",      // Initialize Firebase
   authDomain: "train-scheduler-edf83.firebaseapp.com",
   databaseURL: "https://train-scheduler-edf83.firebaseio.com",
@@ -9,7 +9,7 @@ var config = {
   messagingSenderId: "415415767658"
 };
 firebase.initializeApp(config);
-console.log(firebase);
+//console.log(firebase);
 var database = firebase.database();                       // Create a variable to reference the database.
 $("#train-info").on("click", function(event){             //added button for adding train info
   event.preventDefault();
@@ -18,7 +18,7 @@ $("#train-info").on("click", function(event){             //added button for add
   var destination = $("#destination-input").val().trim();
   var firstTrainTime = $("#first-input").val().trim();
   var frequency = $("#frequency-input").val().trim();
-  var newTrainInfo = {                                    //creates local temp object for holding employee data
+  var newTrainInfo = {                                    //creates local temp object for holding train data
     name: trainName,
     dest: destination,
     time: firstTrainTime,
@@ -26,10 +26,10 @@ $("#train-info").on("click", function(event){             //added button for add
   };
   
   database.ref().push(newTrainInfo);                      //uploads train data to the database
-  console.log(newTrainInfo.name);                    //logs everything to the console
-  console.log(newTrainInfo.dest);
-  console.log(newTrainInfo.time);
-  console.log(newTrainInfo.freq);
+  //console.log(newTrainInfo.name);                       //logs everything to the console
+  //console.log(newTrainInfo.dest);
+  //console.log(newTrainInfo.time);
+  //console.log(newTrainInfo.freq);
   alert("Train Successfully Added");                      //alert
   $("#train-name-input").val("");                         //clears all the text boxes
   $("#destination-input").val("");
@@ -37,38 +37,29 @@ $("#train-info").on("click", function(event){             //added button for add
   $("#frequency-input").val("");
 });
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {         //create firebase event for adding train info to the database and a row in the html when a user adds an entry
-    console.log(childSnapshot.val());
+    //console.log(childSnapshot.val());
     var trainName = childSnapshot.val().name;             //store everything in a variable
     var destination = childSnapshot.val().dest;
     var firstTrainTime = childSnapshot.val().time;
     var frequency = childSnapshot.val().freq;
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTrainTime);
-    console.log(frequency);
+    //console.log(trainName);
+    //console.log(destination);
+    //console.log(firstTrainTime);
+    //console.log(frequency);
+    $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+       frequency );
+
 }
 )
 //Change to html//
-dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
   $("#train-name-input").text(snapshot.val().name);
   $("#destination-input").text(snapshot.val().dest);
   $("#first-input").text(snapshot.val().time);
   $("#frequency-input").text(snapshot.val().freq);
 });
-//work on this
-// Prettify the employee start
-//var startPretty = moment.unix(firstTrainTime).format("MM/DD/YY");
-// Calculate the months worked using hardcore math
-// To calculate the months worked
-//var trainInfoX = moment().diff(moment(firstTrainTime, "X"), "months");
-//console.log(trainInfoX);
-// Calculate the total billed rate
-//var trainInfoXX = trainInfoX * frequency;
-//console.log(trainInfoXX);
-// Add each train's data into the table
-$("#employee-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
-startPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
 
+});
 
 
 
